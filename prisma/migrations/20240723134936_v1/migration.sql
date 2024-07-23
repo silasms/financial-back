8e5820cb7f540270c7b8200a98e5dd88667bea72
silasms <silas.medeiros7@gmail.com>
2024-07-23 10:58:35 -0300

    chore: db migrate v1

---
+++ prisma/migrations/20240723134936_v1/migration.sql
@@ -0,0 +1,27 @@
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('SPENDING', 'EARNINGS');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "movements" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" "Status" NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "movements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
