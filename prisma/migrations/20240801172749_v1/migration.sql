0f02efb517f4b859ed3ae00a672d3ed0ce34944c
silasms <silas.medeiros7@gmail.com>
2024-08-01 14:28:37 -0300

    feat: add relation to user in db

--- prisma/migrations/20240801172749_v1/migration.sql
+++ prisma/migrations/20240801172749_v1/migration.sql
@@ -19,9 +19,13 @@ CREATE TABLE "movements" (
     "value" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
 
     CONSTRAINT "movements_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "movements" ADD CONSTRAINT "movements_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
