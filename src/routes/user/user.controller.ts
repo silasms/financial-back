4dae04bb083dda6c5934376e19c1830877231b83
silasms <silas.medeiros7@gmail.com>
2024-08-01 09:00:28 -0300

    feat: user controller

--- src/routes/user/user.controller.ts
+++ src/routes/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { LoginBodyDTO } from './dto/login-body.dto';
+import { CreateUserBodyDTO } from './dto/create-user-body.dto';
+import { EditUserBodyDTO } from './dto/edit-user-body.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor (private userService: UserService) {}
+
+  @Post('login')
+  async login(@Body() body: LoginBodyDTO) {
+    return await this.userService.login(body)
+  }
+
+  @Post('register')
+  async register(@Body() body: CreateUserBodyDTO) {
+    return await this.userService.createUser(body)
+  }
+
+  @Patch('edit')
+  async edit(@Body() body: EditUserBodyDTO) {
+    return await this.userService.editUser(body)
+  }
+}
